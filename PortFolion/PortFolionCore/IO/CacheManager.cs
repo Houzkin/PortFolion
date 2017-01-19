f6565401715a2db4d37fa7b0957ea8d97d53c9bb
houzkin <houzkin@gmail.com>
2017-01-19 18:00:25 +0900

    キャッシュ

---
+++ PortFolion/PortFolionCore/IO/CacheManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace PortFolion.IO {
+	public class CacheManager {
+		
+		static readonly string _path = AppDomain.CurrentDomain.BaseDirectory + "cache";
+		static string checkPath(string[] path) {
+			if (!path.Any()) throw new ArgumentNullException();
+			return path.Aggregate("", (a, b) => a + Path.DirectorySeparatorChar + b);
+		}
+
+		public static void SaveCache(string data, params string[] path) {
+			using (var sw = new StreamWriter(_path, false)) {
+				sw.Write(data);
+			}
+		}
+		public static void SaveCache<T>(T data, params string[] path) {
+			var seri = new XmlSerializer(typeof(T));
+			using(var fs = new FileStream(checkPath(path), FileMode.Create)) {
+				seri.Serialize(fs, data);
+			}
+		}
+
+		public static string ReadCache(params string[] path) {
+			if (!File.Exists(checkPath(path))) return "";
+			using(var sr = new StreamReader(checkPath(path))) {
+				return sr.ReadToEnd();
+			}
+		}
+		public static T ReadCache<T>(params string[] path) {
+			if (!File.Exists(checkPath(path))) return default(T);
+			var seri = new XmlSerializer(typeof(T));
+			using(var fs = new FileStream(checkPath(path), FileMode.Open)) {
+				return (T)seri.Deserialize(fs);
+			}
+		}
+
+		public static FileInfo GetFileInfo(params string[] path) {
+			return new FileInfo(checkPath(path));
+		}
+		static ISet<string> _cl = new HashSet<string>();
+
+		public static void AddClearList(params string[] path) {
+			_cl.Add(checkPath(path));
+		}
+
+		public static void Clear() {
+			foreach(var p in _cl) {
+				if (!File.Exists(p)) continue;
+				File.Delete(p);
+			}
+			Directory.Delete(_path);
+			_cl.Clear();
+		}
+	}
+}
---
+++ PortFolion/PortFolionCore/Web/Kdb.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Houzkin;
+using System.Net;
+using PortFolion.IO;
+
+namespace PortFolion.Web {
+	public class StockInfo {
+
+	}
+	public enum KdbData { indices,stocks, }
+	public class KdbDataClient {
+		
+		static DateTime DayOfWeekSkip(DateTime date) {
+			switch (date.DayOfWeek) {
+			case DayOfWeek.Sunday:
+				return date.AddDays(-2);
+			case DayOfWeek.Saturday:
+				return date.AddDays(-1);
+			default:
+				return date;
+			}
+		}
+		public static IEnumerable<StockInfo> AcqireInfo(DateTime date,KdbData type, int start = 0,int end = 10) {
+			for(int i= -1*start; i > -1*end; i--) {
+				date = DayOfWeekSkip(date.AddDays(i));
+				var infoStr = getData(date, type);
+			}
+			return Enumerable.Empty<StockInfo>();
+		}
+		static string getData(DateTime date, KdbData type) {
+			string[] path = { "kdb", type.ToString(), date.ToString("yyyy-MM-dd")+".csv" };
+			string data;
+			if (!CacheManager.GetFileInfo(path).Exists) {
+				data = _download(date, type);
+				CacheManager.SaveCache(data, path);
+			}
+			data = CacheManager.ReadCache(path);
+			return data;
+		}
+		static WebClient wc = new WebClient() { Encoding = Encoding.Default };
+		static string _download(DateTime dt, KdbData type) {
+			string url = "http://k-db.com/" + type.ToString() + "/" + dt.ToString("yyyy-MM-dd") + "?download=csv";
+			return wc.DownloadString(url);
+		}
+
+	}
+}
