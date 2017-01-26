d20ab533dcf54decbab937b4251cb5f1f8db8635
houzkin <houzkin@gmail.com>
2017-01-27 06:38:59 +0900

    vm

---
+++ PortFolion/PortFolion/ViewModels/CommonNodeVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Houzkin.Tree;
+using System.Windows.Input;
+using Houzkin.Architecture;
+using PortFolion.Core;
+
+namespace PortFolion.ViewModels {
+	public abstract class CommonNodeVM : ReadOnlyBindableTreeNode<CommonNode, CommonNodeVM> {
+		internal CommonNodeVM(CommonNode model) : base(model) {
+		}
+
+		protected override CommonNodeVM GenerateChild(CommonNode modelChildNode) {
+			throw new NotImplementedException();
+		}
+		public bool IsExpand { get; set; }
+		public abstract void Edit();
+	}
+}
